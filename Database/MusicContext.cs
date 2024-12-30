cbbbec001b13fe996b0963468c0dde6d693e3a42
Richard <richardfelipewt@hotmail.com>
2024-12-30 13:03:03 -0300

    Adicionando os packages para utilização do ORM EntityFramework e configurando a instância do banco de dados

---
+++ Database/MusicContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeMusicas.Database;
+
+internal class MusicContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DINKZ;Database=GerenciadorMusica;User Id=sa;Password=Dev$123;Encrypt=False;");
+    }
+}
